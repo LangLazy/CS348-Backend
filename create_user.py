1329b8de947e08bcd3b2454279029e4f91961fc3
Rahul Gudise <rahul.gudise@gmail.com>
2023-06-22 14:52:50 -0400

    tmp

--- create_user.py
+++ create_user.py
@@ -12,21 +12,21 @@ def insert_user(name, email, hashedpass):
         db = database()
 
         query = ["Select * from user where email = %s"]
-        result = db.execute(query, [name])
-        log.error("initial scan", result)
+        # result = db.execute(query, [name])
+        # log.error("initial scan", result)
 
-        if result != []:
-            return "Invalid email provided, already exists!"
+        # if result != []:
+        #     return "Invalid email provided, already exists!"
 
-        new_uuid = str(uuid.uuid4())
-        query = "Insert into author(author_id, author_name) VALUES(%s, %s)"
-        result = db.execute(query, [new_uuid, name])
-        log.error("author insert", result)
+        # new_uuid = str(uuid.uuid4())
+        # query = "Insert into author(author_id, author_name) VALUES(%s, %s)"
+        # result = db.execute(query, [new_uuid, name])
+        # log.error("author insert", result)
 
-        query = "Insert into user(author_id, email, user_pass) VALUES(%s, %s, %s)"
-        result = db.execute(query, [new_uuid, email, hashedpass])
-        log.error("user insert", result)
-        return "success"
+        # query = "Insert into user(author_id, email, user_pass) VALUES(%s, %s, %s)"
+        # result = db.execute(query, [new_uuid, email, hashedpass])
+        # log.error("user insert", result)
+        # return "success"
 
     except Exception as e:
         log.error(e)
--- server.py
+++ server.py
@@ -31,7 +31,7 @@ def create_user():
     try:
         name = payload['name']
         # password = payload['password']
-        password = hashlib.sha256(payload['password'].encode('utf-8')).hexdigest()
+        password = (hashlib.sha256(payload['password'].encode('utf-8'))).hexdigest()
         email = payload['email']
     except Exception as e:
         app.logger.error("Request with partial payload encountered on /signup")
