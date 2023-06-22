ae8f9541ac9b5f0cbd01dda4eaddede45d12e50b
Rahul Gudise <rahul.gudise@gmail.com>
2023-06-22 17:16:33 -0400

    fixed fk issue

--- add_paper.py
+++ add_paper.py
@@ -16,14 +16,14 @@ def publish_paper(author_id, title, year, fos_name, n_citation, url, page_start,
         result = db.execute((query), [author_id])
         if result == []:
             return "ERROR: No such author exists"
-        
-        query = "Insert into wrote(author_id, paper_id) VALUES(%s, %s)"
-        db.execute((query), [author_id, paper_id], False)
 
         query = '''Insert into paper(paper_id, title, year, fos_name, n_citation, page_start, page_end, doc_type, lang, vol, issue, issn, isbn, doi, url, abstract)
                                VALUES(%s, %s, %s, %s, %d, %d, %d, %s, %s, %d, %d, %s, %s, %s, %s, %s)'''
         db.execute((query), [paper_id, title, year, fos_name, n_citation, page_start, page_end, 
                                 doc_type, lang, vol, issue, issn, isbn, doi, url, abstract], False)
+
+        query = "Insert into wrote(author_id, paper_id) VALUES(%s, %s)"
+        db.execute((query), [author_id, paper_id], False)
 
         return "success"
 
