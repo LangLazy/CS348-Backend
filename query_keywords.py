758f99b5331fe474fae896f16ed3de86e830bbc0
Rahul Gudise <rahul.gudise@gmail.com>
2023-06-22 10:51:42 -0400

    fixed env var issue

--- query_keywords.py
+++ query_keywords.py
@@ -3,7 +3,7 @@ import os
 
 def query_keywords(keywords: list[str]):
     try:
-        USER = os.getenv('SQL_USERNAME')
+        USER = os.getenv('SQL_USER')
         PASSWORD = os.getenv('SQL_PASSWORD')
         HOST = os.getenv('SQL_HOST')
         PORT = os.getenv('SQL_PORT')
