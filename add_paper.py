3d1845f2bbe9a128760bf8733d1c7a2f185f76fb
Rahul Gudise <rahul.gudise@gmail.com>
2023-06-22 17:15:24 -0400

    fixed inccorect sql query

--- add_paper.py
+++ add_paper.py
@@ -12,17 +12,17 @@ def publish_paper(author_id, title, year, fos_name, n_citation, url, page_start,
         db = database()
         paper_id = uuid.uuid4().hex
 
-        query = "Select * from author with author_id = %s"
-        result = db.execute(query, [author_id])
+        query = "Select * from author where author_id = %s"
+        result = db.execute((query), [author_id])
         if result == []:
             return "ERROR: No such author exists"
         
         query = "Insert into wrote(author_id, paper_id) VALUES(%s, %s)"
-        db.execute(query, [author_id, paper_id], False)
+        db.execute((query), [author_id, paper_id], False)
 
         query = '''Insert into paper(paper_id, title, year, fos_name, n_citation, page_start, page_end, doc_type, lang, vol, issue, issn, isbn, doi, url, abstract)
                                VALUES(%s, %s, %s, %s, %d, %d, %d, %s, %s, %d, %d, %s, %s, %s, %s, %s)'''
-        db.execute(query, [paper_id, title, year, fos_name, n_citation, page_start, page_end, 
+        db.execute((query), [paper_id, title, year, fos_name, n_citation, page_start, page_end, 
                                 doc_type, lang, vol, issue, issn, isbn, doi, url, abstract], False)
 
         return "success"
