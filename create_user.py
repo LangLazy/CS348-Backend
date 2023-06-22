5e5c75e65e720bfdb9b380ea11b8650d7c5cd16d
Rahul Gudise <rahul.gudise@gmail.com>
2023-06-22 14:58:16 -0400

    tmp

--- create_user.py
+++ create_user.py
@@ -11,23 +11,23 @@ def insert_user(name, email, hashedpass):
         log = logging.getLogger(__name__)
         db = database()
         log.debug("DONE 0")
-        query = ["Select * from user where email = %s"]
-        result = db.execute(query, [name])
-        log.debug("DONE 1")
+        # query = ["Select * from user where email = %s"]
+        # result = db.execute(query, [name])
+        # log.debug("DONE 1")
 
-        if result != []:
-            return "Invalid email provided, already exists!"
+        # if result != []:
+        #     return "Invalid email provided, already exists!"
 
-        new_uuid = str(uuid.uuid4())
-        query = "Insert into author(author_id, author_name) VALUES(%s, %s)"
-        result = db.execute(query, [new_uuid, name])
-        log.debug("DONE 2")
-        log.error("author insert", result)
+        # new_uuid = str(uuid.uuid4())
+        # query = "Insert into author(author_id, author_name) VALUES(%s, %s)"
+        # result = db.execute(query, [new_uuid, name])
+        # log.debug("DONE 2")
+        # log.error("author insert", result)
 
-        query = "Insert into user(author_id, email, user_pass) VALUES(%s, %s, %s)"
-        result = db.execute(query, [new_uuid, email, hashedpass])
-        log.debug("DONE 3")
-        log.error("user insert", result)
+        # query = "Insert into user(author_id, email, user_pass) VALUES(%s, %s, %s)"
+        # result = db.execute(query, [new_uuid, email, hashedpass])
+        # log.debug("DONE 3")
+        # log.error("user insert", result)
         return "success"
 
     except Exception as e:
